c810a5bc224483289fb66f5dc4aefd05b4f8d0de
alialhaidar2012-netizen <ali.alhaidar2012@gmail.com>
2026-06-09 19:08:28 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,22 @@
 const CACHE_NAME = 'gamecombo-v1';
-const urlsToCache=[
-'/GameCombo/',
-'/GameCombo/index.html',
-'/GameCombo/manifest.json',
-'/GameCombo/icon512_maskable.png',
-'/GameCombo/icon512_rounded.png'
+const urlsToCache = [
+  '/GameCombo/',
+  '/GameCombo/index.html',
+  '/GameCombo/manifest.json',
+  '/GameCombo/icon512_maskable.png',
+  '/GameCombo/icon512_rounded.png'
 ];
+
+// Installation: Cache speichern
 self.addEventListener('install', event => {
   event.waitUntil(
-   caches.open(CACHE_NAME)
-    .then(cache => cache.addAll(urlsToCache))
-    ));
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
 });
+
+// Fetch: Bei Offline aus Cache bedienen
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-    .then(response => || fetch(event.request))
-  ));
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
 });
